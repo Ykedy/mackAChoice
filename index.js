172c6a0352651777ca118b28af8b34d662961872
Ykedy <loroidyun@outlook.com>
2021-10-21 15:21:05 +0800

    新增大转盘自定义选项、提示

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const playPoker = document.querySelector(".playPoker");
 const wheelBody = document.querySelector('.wheelBody');
 const twoBody = document.querySelector('.twoBody');
 const pokerBody = document.querySelector('.pokerBody');
-
+const promptBox = document.querySelector(".prompt")
 /* nav栏切换start */
 fortuneWheel.addEventListener('click', function () {
     wheelBody.style.display = 'flex'
@@ -25,18 +25,21 @@ playPoker.addEventListener('click', function () {
 /* nav栏切换end */
 // 大转盘区域
 /* 大转盘start */
-const wheelTexts = document.querySelectorAll('.wheelText');
-const textBtn = document.querySelector('.textBtn');
-const textDeleteBtn = document.querySelector('.textDeleteBtn');
+
+
+
 const canvas = document.getElementById('tutorial');
 const btn = document.getElementById('btn');
 const customBox = document.querySelector(".customBox");
-const saveCustom = document.querySelector(".saveCustom");
+
 const inputTitle = document.querySelector(".inputTitle");
 const saveTitle = document.querySelector(".saveTitle");
 const unsaveTitle = document.querySelector(".unsaveTitle");
 const titleAlert = document.querySelector(".titleAlert")
 const shadow = document.querySelector(".shadow")
+const inputWheel = document.querySelector(".inputWheel")
+const optionBox = document.querySelector(".optionBox")
+const btnShowOption = document.querySelector(".btnShowOption")
 var rotateTimes = 1;
 var saveRandom = 0;
 var flag = false;
@@ -44,6 +47,7 @@ var dataBackup = [];
 var customTitleName = '';
 var dbData = {};
 var delBtn = []
+var wheelTexts, optionNum
 // 自定义转盘的每一项
 var customContent = [];
 const dataWheel = {
@@ -60,34 +64,147 @@ utools.onPluginReady(() => {
     }
     if (utools.db.allDocs()) {
         updateCustom()
-        // deleteCustom()
 
     }
 }
 )
+var btnsHtml = '<li class="textBtn"><span class="iconfont icon-check_circled"></span>确&nbsp;&nbsp;定</li><li class="textDeleteBtn" > <span class="iconfont icon-quxiao"></span>清&nbsp;&nbsp;除</li ><li class="saveCustom">保&nbsp;存&nbsp;自&nbsp;定&nbsp;义</li>'
+// 点击确定出现一堆输入框，输入选项内容
+btnShowOption.addEventListener('click', function () {
+
+    optionNum = inputWheel.value;
+    if (optionNum <= 18 && optionNum >= 2) { addOptionItem() }
+    
+
+})
+// 添加选项，并添加事件
+function addOptionItem(){
+    
+        optionBox.innerHTML = btnsHtml
+        for (let i = 0; i < optionNum; i++) {
+            optionBox.innerHTML = '<li><span class="iconfont icon-daohangquanzi"></span><input type="text" class="wheelText" placeholder = "写点什么.." ></li >' + optionBox.innerHTML
+        };
+        wheelTexts = document.querySelectorAll('.wheelText');
+        //生成画布
+        var textBtn = document.querySelector('.textBtn');
+        textBtn.addEventListener('click', function () {
+            var ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            filterContent(wheelTexts.length)
+            draw(dataBackup);
+        })
+        //清除内容
+        var textDeleteBtn = document.querySelector('.textDeleteBtn');
+        textDeleteBtn.addEventListener('click', function () {
+            for (let i = 0; i < optionNum; i++)
+                wheelTexts[i].value = ''
+        })
+        var saveCustom = document.querySelector(".saveCustom");
+        // 点击出现输入框，输入主题名字 
+        saveCustom.addEventListener("click", function () {
+            titleAlert.style.display = "flex";
+            shadow.style.display = "block";
+        })
+
+
+   
+}
+// 筛选传入的文字
+function filterContent(num) {
+    var moreLength = 0
+    var data = []
+    var spaceItem = 0
+    dataBackup = [];
+    wheelTexts.forEach((item) => {
+        data.push(item.value)
+    })
+    data.forEach((item) => {
+        if (item.trim() === '') {
+            spaceItem++;
+
+        } else {
+            dataBackup.push(item)
+            moreLength = data.length - dataBackup.length
+
+        }
+    })
+    // 如果全部都没写
+    if (spaceItem===num) {
+        for (let i = 0; i < optionNum; i++) { dataBackup.push(' ') }
+    }
+    // 当有部分内容未填写时，随机填写
+    else if (moreLength > 0) {
+        let arr = [...dataBackup]
+        for (let addIndex = moreLength - 1; addIndex >= 0; addIndex--) {
+            dataBackup.push(arr[Math.floor(Math.random() * arr.length)])
+        }
+    }
+}
+
 //扇形
 function draw(text) {
     const length = 400
     const radius = length / 2
     const PI2 = Math.PI * 2
-    const num = 6;
+    const num = text.length;
     var canvas = document.getElementById('tutorial');
+    //防止文字溢出
+    function getTextList(ctx, text, maxLineWidth) {
+        // words是把文字分开
+        var words = text.split('');
+        var tempLine = '';
+        var list = [];
+        // 一个字一个字的进行循环
+        for (let i = 0; i < words.length; i++) {
+            // 第一步：测量空字符串的宽度，如果大于你传入的最大字符串宽度的话，当然不可能，去看第二步
+            // 第三步：加到xxx时，长度大于你传入的长度了，进入if
+            if (ctx.measureText(tempLine).width >= maxLineWidth) {
+                // 第四步：在空数组中，追加tempLine（此时是一串字符串）
+                list.push(tempLine);
+                // 第五步：重新设定传入的maxLineWidth，它减去最原始的第一个字符的宽度
+                maxLineWidth -= ctx.measureText(text[0]).width;
+                tempLine = ''
+            }
+            // 第二步：templine字符串加入第一个字，去看第三步
+            // 第六步：一直到最后一个字符，全部结束后，跳出if，此时得到了一个数组（list）和一个字符串（tempLine）
+            tempLine += words[i]
+        }
+        // 在数组中追加字符串tempLine，得到了一个数组，里面存放着分完行的字符串
+        list.push(tempLine);
+        return list
+    }
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
         //计算每个奖项所占角度数
         var baseAngle = PI2 / num;
-        ctx.clearRect(0, 0, length, length);//去掉背景默认的黑色
-        // colors = ["#5db641", "#eddd97"];
-        colors = ["#e3f1fe", "#0078fc"];
+        // 一行文字的最大长度
+        var textMaxWidth = 0;
+        // 文字的位置（半径）
+        var textRadius = 150;
+        var colorBox = ['#0078fc', '#e3f1fe', '#a2c5ea']
+        ctx.save();
         for (var index = 0; index < num; index++) {
             var angle = index * baseAngle;
-            if ((index + 1) % 2 !== 0) {
+            if (num % 2 === 1) {
+                if ((index + 1) % 2 != 0) {
+                    ctx.fillStyle = colorBox[0];//设置每个扇形区域的颜色
 
-                ctx.fillStyle = colors[0];//设置每个扇形区域的颜色
-            }
-            else {
-                ctx.fillStyle = colors[1];//设置每个扇形区域的颜色
+                } else {
+                    ctx.fillStyle = colorBox[1];//设置每个扇形区域的颜色
 
+                }
+                if (num === index + 1) {
+
+                    ctx.fillStyle = colorBox[2];//设置每个扇形区域的颜色
+                }
+            } else {
+                if ((index + 1) % 2 === 0) {
+
+                    ctx.fillStyle = colorBox[0];//设置每个扇形区域的颜色
+                } else {
+                    ctx.fillStyle = colorBox[1];//设置每个扇形区域的颜色
+
+                }
             }
             ctx.beginPath();//开始绘制
 
@@ -96,51 +213,31 @@ function draw(text) {
             ctx.fill();//填充颜色
             ctx.save();//保存当前环境的状态
             ctx.beginPath();
-            // ctx.fillStyle = '#574e1a'
-            ctx.font = '20px Microsoft YaHei';
-
-            if (index === 0) {
-                ctx.fillStyle = '#0078fc'
-                ctx.translate(250, 238);
-                ctx.rotate(PI2 / 12);
-                ctx.fillText(text[index], 0, 0)
-
-
+            ctx.fillStyle = '#000';
+            ctx.font = '18px Microsoft YaHei';
+            // 文字换行长度
+            if (num < 5) {
+                textMaxWidth = 120
+                textRadius = 135
+            } else if (num >= 5 && num < 8) {
+                textMaxWidth = 90
+            } else if (num >= 8 && num < 12) {
+                textMaxWidth = 65
+            } else {
+                ctx.font = '16px Microsoft YaHei';
+                textMaxWidth = 50
             }
-            if (index === 1) {
-                ctx.fillStyle = '#e3f1fe'
-                ctx.translate(190, 266)
-                ctx.rotate(PI2 / 4);
-                ctx.fillText(text[index], 0, 0)
-
-            }
-            if (index === 2) {
-                ctx.fillStyle = '#0078fc'
-                ctx.translate(140, 230)
-                ctx.rotate(PI2 / 2.4);
-                ctx.fillText(text[index], 0, 0)
-
-            }
-            if (index === 3) {
-                ctx.fillStyle = '#e3f1fe'
-                ctx.translate(150, 154)
-                ctx.rotate(PI2 / 1.7);
-                ctx.fillText(text[index], 0, 0)
-            }
-            if (index === 4) {
-                ctx.fillStyle = '#0078fc'
-                ctx.translate(205, 130)
-                ctx.rotate(PI2 * 3 / 4);
-                ctx.fillText(text[index], 0, 0)
-            }
-            if (index === 5) {
-                ctx.fillStyle = '#e3f1fe'
-                ctx.translate(260, 170)
-                ctx.rotate(PI2 * 12 / 13);
-                ctx.fillText(text[index], 0, 0)
-            }
-
-            ctx.fill()
+            // 改变原点
+            ctx.translate(
+                radius + Math.cos(angle + baseAngle / 2) * textRadius,
+                radius + Math.sin(angle + baseAngle / 2) * textRadius
+            );
+            // 旋转角度
+            ctx.rotate(angle + baseAngle / 2 + Math.PI / 2);
+            // 文字
+            getTextList(ctx, text[index], textMaxWidth).forEach((line, i) => {
+                ctx.fillText(line, -ctx.measureText(line).width / 2, ++i * 25)
+            });
             ctx.restore();
         }
         baseContent(ctx, radius, PI2);
@@ -254,69 +351,19 @@ btn.addEventListener('click', function () {
         }, timer * 1000 + 20)
     }
 })
-// 筛选传入的6项文字
-function filterContent() {
-    var moreLength = 0
-    var data = []
-    var surplus = 0;
-    dataBackup = [];
-    wheelTexts.forEach((item) => {
-        data.push(item.value)
-    })
-    data.forEach((item) => {
-        if (item.trim() === '') {
-            surplus++
-
-        } else {
-            dataBackup.push(item)
-            moreLength = data.length - dataBackup.length
-
-        }
-    })
-    // 如果全部都没写
-    if (surplus === 6) {
-        dataBackup = ['', '', '', '', '', '']
-    }
-    // 当有部分内容未填写时，随机填写
-    if (moreLength > 0) {
-        let arr = [...dataBackup]
-        for (let addIndex = moreLength - 1; addIndex >= 0; addIndex--) {
-            dataBackup.push(arr[Math.floor(Math.random() * arr.length)])
-        }
-    }
-}
-//修改文字
-textBtn.addEventListener('click', function () {
-    var ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    filterContent()
-    draw(dataBackup);
-})
-//清除内容
-textDeleteBtn.addEventListener('click', function () {
-    for (let i = 0; i < 6; i++)
-        wheelTexts[i].value = ''
-})
 //预设选项
 //点击切换大转盘内容
 function changeContent(content) {
     draw(content);
-    content.forEach((item, index) => {
-        wheelTexts[index].value = item
-    })
+    optionNum = content.length
+    addOptionItem()
+    for(let i =0 ;i<optionNum;i++){
+        wheelTexts[i].value = content[i]
+
+    }
 
 }
-// 点击出现输入框，输入主题名字 
-saveCustom.addEventListener("click", function () {
-    titleAlert.style.display = "flex";
-    shadow.style.display = "block";
-})
-// 点击取消
-unsaveTitle.addEventListener("click", function () {
-    titleAlert.style.display = "none";
-    shadow.style.display = "none";
-    inputTitle.value = ""
-})
+
 var customItem = []
 
 // 更新
@@ -335,6 +382,12 @@ function updateCustom() {
     })
     deleteCustom()
 }
+// 点击取消
+unsaveTitle.addEventListener("click", function () {
+    titleAlert.style.display = "none";
+    shadow.style.display = "none";
+    inputTitle.value = ""
+})
 //点击保存预设名字
 saveTitle.addEventListener("click", function () {
     customTitleName = inputTitle.value;
@@ -342,15 +395,17 @@ saveTitle.addEventListener("click", function () {
     for (i of dbData) {
         objLengths++
     }
-    if (utools.dbStorage.getItem(customTitleName) || customTitleName === '') {
+    if (objLengths > 7) {
+        promptMessage("已经有八个预设了，如需新增，请删除不需要的。");
         return
     }
-    else if (objLengths > 7) {
+    else if (utools.dbStorage.getItem(customTitleName) || customTitleName === '') {
+        promptMessage("名字不可重复，也不可为空。");
         return
     }
     else {
         customBox.innerHTML = ''
-        filterContent()
+        filterContent(wheelTexts.length)
         utools.dbStorage.setItem(customTitleName, dataBackup)
         // 保存完毕后，将此标题放到预设框中显示出来
         updateCustom()
@@ -369,9 +424,20 @@ function deleteCustom() {
             updateCustom()
         })
     })
+}
+// 提示
+var promotFlag = 0;
+function promptMessage(message) {
+    if (promotFlag === 0) {
+        promptBox.style.display = "inline-block"
+        promptBox.innerHTML = message
+        promotFlag = 1
 
-
-
+        setTimeout(() => {
+            promptBox.style.display = "none"
+            promotFlag = 0
+        }, 2000)
+    }
 }
 /* 大转盘end */
 
@@ -459,3 +525,4 @@ toShowCardbtn.addEventListener('click', function () {
 
 })
 /* 抽卡 end */
+
